d63917c214ba59d7809b3354c6d72e1e7d8c1c39
Ruan Carlos <ruancarlosporto@hotmail.com>
2018-04-29 13:27:42 -0300

    Funções ListaAdjacencia

--- matrizEListaDeAdjacencia/matrizEListaDeAdjacencia/ListaAdjacencia.cs
+++ matrizEListaDeAdjacencia/matrizEListaDeAdjacencia/ListaAdjacencia.cs
@@ -23,8 +23,109 @@ namespace matrizEListaDeAdjacencia
 
         public bool InserirVertice(int vertice)
         {
+            if (LA.Contains(vertice))
+            {
+                return false;
+            }
             LA.Add(vertice);
             return true;
+        }
+
+        public bool RemoverVertice(int vertice)
+        {
+            if (VerificaExistenciaVertice(vertice))
+            {
+                LA.Remove(vertice);
+                return true;
+            }
+            throw new ArgumentException();
+        }
+
+        public bool InserirAresta(int v1, int v2)
+        {
+            if(VerificaExistenciaVertice(v1) && VerificaExistenciaVertice(v2))
+            {
+                if (VerificaExistenciaAresta(v1, v2) || VerificaExistenciaAresta(v2,v1))
+                    return false;
+
+                string valorV1 = LA[v1].ToString();
+                valorV1 += (v2+",");
+                LA.Insert(v1, valorV1);
+
+                string valorV2 = LA[v2].ToString();
+                valorV2 += (v1+",");
+                LA.Insert(v2, valorV2);
+
+                return true;
+            }
+            throw new ArgumentException();
+        }
+
+        public bool RemoverAresta(int v1, int v2)
+        {
+            if (VerificaExistenciaVertice(v1) && VerificaExistenciaVertice(v2))
+            {
+                if (!VerificaExistenciaAresta(v1, v2) || !VerificaExistenciaAresta(v2, v1))
+                    return false;
+
+                string valorV1 = LA[v1].ToString();
+                string[] valoresV1 = valorV1.Split(',');
+                valorV1 = "";
+                for(int i = 0; i < (valoresV1.Length - 1); i++)
+                {
+                    if(valoresV1[i] != (v2 + ""))
+                    {
+                        valorV1 += valoresV1[i] + ",";
+                    }
+
+                }
+                LA.RemoveAt(v1);
+                LA.Add(v1);
+                LA.Insert(v1, valorV1);
+
+                string valorV2 = LA[v2].ToString();
+                string[] valoresV2 = valorV2.Split(',');
+                valorV2 = "";
+                for (int i = 0; i < (valoresV2.Length - 1); i++)
+                {
+                    if (valoresV2[i] != (v1 + ""))
+                    {
+                        valorV2 += valoresV2[i] + ",";
+                    }
+
+                }
+                LA.RemoveAt(v2);
+                LA.Add(v2);
+                LA.Insert(v2, valorV2);
+                return true;
+            }
+            throw new ArgumentException();
+        }
+
+        public int Grau(int vertice)
+        {
+            string itens = LA[vertice].ToString();
+            string[] numItens = itens.Split(',');
+            return (numItens.Length - 1);
+        }
+
+        private bool VerificaExistenciaVertice(int vertice)
+        {
+            if (LA.Contains(vertice))
+                return true;
+            return false;
+        }
+
+        private bool VerificaExistenciaAresta(int v1, int v2)
+        {
+            string valorV1 = LA[v1].ToString();
+            string[] valoresV1 = valorV1.Split(',');
+            for(int i = 0; i < valoresV1.Length; i++)
+            {
+                if (valoresV1[i] == (v2 + ""))
+                    return true;
+            }
+            return false;
         }
     }
 }
--- matrizEListaDeAdjacencia/matrizEListaDeAdjacencia/Program.cs
+++ matrizEListaDeAdjacencia/matrizEListaDeAdjacencia/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace matrizEListaDeAdjacencia
 {
@@ -10,7 +11,21 @@ namespace matrizEListaDeAdjacencia
     {
         static void Main(string[] args)
         {
-            //COMMIT INICIAL
+            int v1, v2;
+            v1 = 32;
+            v2 = 20;
+            ArrayList a = new ArrayList();
+            a.Add(1);
+            a.Insert(1, v1 + "," + v2);
+            string val = a[1].ToString();
+            string[] valresult = val.Split(',');
+            a.Insert(1, valresult[0]);
+            //a.Insert(1, v2);
+            //string val = a[1].
+            //Console.Write(a[1].ToString());
+           // a.RemoveAt(1);
+            Console.Write(a[1].ToString());
+            Console.ReadKey(true);
 
         }
     }
