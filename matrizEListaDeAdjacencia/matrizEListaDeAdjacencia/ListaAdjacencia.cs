a152efc94b272358d925293e41948bb213a70e48
Ruan Carlos <ruancarlosporto@hotmail.com>
2018-04-29 11:59:08 -0300

    Inserção LA

---
+++ matrizEListaDeAdjacencia/matrizEListaDeAdjacencia/ListaAdjacencia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace matrizEListaDeAdjacencia
+{
+    class ListaAdjacencia
+    {
+        private ArrayList LA;
+
+        public ListaAdjacencia()
+        {
+            LA = new ArrayList();
+        }
+
+        public int Ordem()
+        {
+            return LA.Count;
+        }
+
+        public bool InserirVertice(int vertice)
+        {
+            LA.Add(vertice);
+            return true;
+        }
+    }
+}
