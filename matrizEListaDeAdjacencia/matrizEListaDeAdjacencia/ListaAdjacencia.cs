a8da3bfd827d32981d1d964c5ecc70b96138cb98
Ruan Carlos <ruancarlosporto@hotmail.com>
2018-05-01 10:13:22 -0300

    Fim Lista Adjacencia

--- matrizEListaDeAdjacencia/matrizEListaDeAdjacencia/ListaAdjacencia.cs
+++ matrizEListaDeAdjacencia/matrizEListaDeAdjacencia/ListaAdjacencia.cs
@@ -10,6 +10,8 @@ namespace matrizEListaDeAdjacencia
     class ListaAdjacencia
     {
         private ArrayList LA;
+        private string verticesLA = "";
+        //private string[,] LA;
 
         public ListaAdjacencia()
         {
@@ -27,6 +29,7 @@ namespace matrizEListaDeAdjacencia
             {
                 return false;
             }
+            verticesLA += vertice + ",";
             LA.Add(vertice);
             return true;
         }
@@ -43,17 +46,17 @@ namespace matrizEListaDeAdjacencia
 
         public bool InserirAresta(int v1, int v2)
         {
-            if(VerificaExistenciaVertice(v1) && VerificaExistenciaVertice(v2))
+            if (VerificaExistenciaVertice(v1) && VerificaExistenciaVertice(v2))
             {
-                if (VerificaExistenciaAresta(v1, v2) || VerificaExistenciaAresta(v2,v1))
+                if (VerificaExistenciaAresta(v1, v2) || VerificaExistenciaAresta(v2, v1))
                     return false;
 
                 string valorV1 = LA[v1].ToString();
-                valorV1 += (v2+",");
+                valorV1 += (v2 + ",");
                 LA.Insert(v1, valorV1);
 
                 string valorV2 = LA[v2].ToString();
-                valorV2 += (v1+",");
+                valorV2 += (v1 + ",");
                 LA.Insert(v2, valorV2);
 
                 return true;
@@ -71,9 +74,9 @@ namespace matrizEListaDeAdjacencia
                 string valorV1 = LA[v1].ToString();
                 string[] valoresV1 = valorV1.Split(',');
                 valorV1 = "";
-                for(int i = 0; i < (valoresV1.Length - 1); i++)
+                for (int i = 0; i < (valoresV1.Length - 1); i++)
                 {
-                    if(valoresV1[i] != (v2 + ""))
+                    if (valoresV1[i] != (v2 + ""))
                     {
                         valorV1 += valoresV1[i] + ",";
                     }
@@ -109,6 +112,99 @@ namespace matrizEListaDeAdjacencia
             return (numItens.Length - 1);
         }
 
+        public bool Completo()
+        {
+            string adjacentes = "";
+            string[] adjacentesV;
+            string[] vertices = this.verticesLA.Split(',');
+            ArrayList adjacentesA = new ArrayList();
+            for(int i = 0; i < (vertices.Length - 1); i++)
+            {
+                adjacentes = LA[i].ToString();
+                adjacentesV = adjacentes.Split(',');
+                for(int j = 0; j < adjacentesV.Length - 1; j++)
+                {
+                    adjacentesA.Add(adjacentesV[j]);
+                }
+                foreach(string s in vertices)
+                {
+                    if (!adjacentesA.Contains(s))
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public bool Regular()
+        {
+            string adjacentes = LA[0].ToString();
+            string adjacentesAux;
+            string[] adjacentesV = adjacentes.Split(',');
+            string[] adjacentesVAux;
+            int numArest = adjacentesV.Length;
+            for(int i = 1; i < LA.Count; i++)
+            {
+                adjacentesAux = LA[i].ToString();
+                adjacentesVAux = adjacentesAux.Split(',');
+                if (adjacentesVAux.Length != numArest)
+                    return false;
+            }
+            return true;
+        }
+
+        public void ShowLA()
+        {
+            for (int i = 0; i < LA.Count; i++)
+                Console.WriteLine(i + " : " + LA[i].ToString());
+        }
+
+        public void SequenciaGraus()
+        {
+            int[] sequenciaGraus = new int[LA.Count];
+            for (int i = 0; i < LA.Count; i++)
+                sequenciaGraus[i] = Grau(i);
+
+            Array.Sort(sequenciaGraus);
+            foreach (int i in sequenciaGraus)
+                Console.Write(i + ",");
+        }
+
+        public void VerticesAdjacentes(int vertice)
+        {
+            Console.WriteLine(LA[vertice].ToString());
+        }
+
+        public bool Isolado(int vertice)
+        {
+            if (Grau(vertice) == 0)
+                return true;
+            return false;
+        }
+
+        public bool Impar(int vertice)
+        {
+            if (Grau(vertice) % 2 != 0)
+                return true;
+            return false;
+        }
+
+        public bool Par(int vertice)
+        {
+            if (Grau(vertice) % 2 == 0)
+                return true;
+            return false;
+        }
+
+        public bool Adjacentes(int v1, int v2)
+        {
+            string itens = LA[v1].ToString();
+            string[] itensSep = itens.Split(',');
+            for (int i = 0; i < itensSep.Length - 1; i++)
+                if (itensSep[i] == (v2 + ""))
+                    return true;
+            return false;
+        }
+
         private bool VerificaExistenciaVertice(int vertice)
         {
             if (LA.Contains(vertice))
@@ -120,7 +216,7 @@ namespace matrizEListaDeAdjacencia
         {
             string valorV1 = LA[v1].ToString();
             string[] valoresV1 = valorV1.Split(',');
-            for(int i = 0; i < valoresV1.Length; i++)
+            for (int i = 0; i < valoresV1.Length - 1; i++)
             {
                 if (valoresV1[i] == (v2 + ""))
                     return true;
